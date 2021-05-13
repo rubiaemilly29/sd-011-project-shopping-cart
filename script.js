12843479761e87f4348a92f474a022a80fc3b4ee
Gisele Costa <jikinha@gmail.com>
2021-05-13 15:31:44 -0300

    requisito 1

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -14,7 +12,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -41,3 +39,24 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+const fetchItems = () => {
+  const options = {};
+  const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  return fetch(url, options)
+    .then((response) => response.json())
+    .then(({ results }) => results)
+    .catch((error) => console.error(error));
+};
+
+const addItems = async () => {
+  const items = await fetchItems();
+
+  const itemsContainer = document.querySelector('section.items');
+
+  items.forEach((item) => itemsContainer.appendChild(createProductItemElement(item)));
+};
+
+window.onload = function onload() {
+  addItems();
+};
