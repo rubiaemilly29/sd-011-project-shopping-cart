008de97ac3416a838d205fa7eb545b7f440f9d91
Marcelo Araujo <almeidadearaujomarcelo@gmail.com>
2021-05-13 17:10:56 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -28,24 +28,34 @@ function createProductItemElement({ sku, name, image }) {
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  // li.addEventListener('click', cartItemClickListener);
+  return li;
+}
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+  const product = event.target.parentElement;
+  fetch(`https://api.mercadolibre.com/items/${product.firstChild.innerText}`)
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementsByTagName('ol')[0].appendChild(createCartItemElement(
+        ({ sku: data.id,
+          name: data.title,
+          salePrice: data.price }),
+      ));
+    });
+}
 
 window.onload = function onload() {
   fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
     .then((response) => response.json())
     .then((data) => { 
-      console.log(data);
-      console.log(typeof data);
+      // console.log(data);
+      // console.log(typeof data);
       for (let index = 0; index < data.paging.limit; index += 1) {
         document.getElementsByClassName('items')[0].appendChild(createProductItemElement(
           ({ sku: data.results[index].id,
@@ -56,4 +66,5 @@ window.onload = function onload() {
     });
   // console.log(fetchResult);
   // createProductItemElement(fetchResult);
+  document.getElementsByClassName('items')[0].addEventListener('click', cartItemClickListener);
 };
