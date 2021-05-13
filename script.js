1b2c7e62ddfe3b98b06d8198d0b54d6c110bceee
Rodolfo <rodolforrcont@gmail.com>
2021-05-13 17:21:34 -0300

    Requisito 4

--- script.js
+++ script.js
@@ -12,13 +12,21 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+function cartItemClickListener(event, cartItems, count) {
+  // coloque seu código aqui
+  localStorage.removeItem(`product${count}`);
+  cartItems.removeChild(event.target);
+}
+
 function createCartItemElement({ sku, name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   const cartItems = document.querySelector('.cart__items');
-  cartItems.appendChild(li)
-  .addEventListener('click', (event) => cartItems.removeChild(event.target));
+  localStorage.setItem(`product${cartItems.childElementCount}`, `${sku},${name},${salePrice}`);
+  const count = cartItems.childElementCount;
+  cartItems.appendChild(li);
+  li.addEventListener('click', (event) => cartItemClickListener(event, cartItems, count));
   return li;
 }
 
@@ -40,7 +48,14 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image, pric
 const getProduct = (term) => {
   fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${term}`)
     .then((response) => response.json())
-    .then((response) => response.results.forEach((computer) => createProductItemElement(computer)));
+    .then((response) => response.results.forEach((computer) => createProductItemElement(computer)))
+    .then(() => {
+      for (let index = 0; index < localStorage.length; index += 1) {
+        const [sku, name, price] = (localStorage.getItem(`product${index}`).split(','));
+        const objProduct = { sku, name, price };
+        createCartItemElement(objProduct);
+    }
+  });
 };
 
 const fetchProduct = () => {
@@ -49,10 +64,6 @@ const fetchProduct = () => {
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  // coloque seu código aqui
 }
 
 window.onload = function onload() { 
