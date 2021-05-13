63399799652b75d3c9f5c668de6b29e685a532ec
Gustavo Lemes <gustavolemes997@gmail.com>
2021-05-13 14:22:27 -0300

    Iniciando

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  // code aqui
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
