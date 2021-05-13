05a4a6043468e56abc07f50c305b7064c4586d96
josanjohnata <josanjohnata@gmail.com>
2021-05-13 15:17:32 -0300

    requisito 1

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -14,7 +12,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement(sku, name, image) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -34,10 +32,59 @@ function cartItemClickListener(event) {
   // coloque seu código aqui
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ id, title, price }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.innerText = `SKU: ${id} | NAME: ${title} | PRICE: $${price}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+const fetchID = (id) => {
+  fetch(`https://api.mercadolibre.com/items/${id}`)
+    .then((response) => response.json())
+    .then((data) => {;
+      const setId = data;
+      const cartItem = '.cart__items';
+      document.querySelector(cartItem).appendChild(createCartItemElement(setId));
+  }); 
+};
+
+const pickCart =() => {
+  const buttonAddPickCar = document.querySelectorAll('.item__add');
+  buttonAddPickCar.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      const getId = getSkuFromProductItem(event.target.parentElement);
+      fetchId(getId);
+    });
+  });
+};
+
+const fetchCurrency = () => {
+  const endPoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  return new Promise((resolve, reject) => {
+    fetch(endPoint)
+      .then((response) => response.json())
+      .then((object) => {
+        resolve(object.results);
+        if (object.results.error) {
+          throw new Error(object.results.error);
+        }
+      }).catch((error) => reject(window.alert(error)));
+  });
+};
+
+const fetchCurrencyAsyncAwait = async () => {
+  const data = await fetchCurrency();
+  data.forEach(object => {
+    const productElement = createProductItemElement(
+      object.id, object.title, object.thumbnail
+    );
+    document.querySelector('.items').appendChild(productElement);
+  });
+};
+
+window.onload = function onload() {
+  fetchCurrencyAsyncAwait();
+  pickCart();
+};
