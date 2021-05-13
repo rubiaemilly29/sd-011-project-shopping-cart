4aedfb0aa1e329bbcbd6821157086d445929606a
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-05-13 17:57:28 -0300

    First commitada - requisito 1 - lint

--- script.js
+++ script.js
@@ -29,15 +29,15 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+//function getSkuFromProductItem(item) {
+  //return item.querySelector('span.item__sku').innerText;
+//}
 
-function cartItemClickListener(event) {
+//function cartItemClickListener(event) {
   // coloque seu código aqui
-}
+//}
 
-function createCartItemElement({ sku, name, salePrice }) {
+//function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
