527088b8ba357bff436e2d3a9a1e3db1a55aad2c
Leandro Lopes <leandro_carlos@uol.com.br>
2021-05-13 20:57:40 -0300

    Iniciando o projeto

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
 }
-// 
+ 
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
 }
+
+const searchComputer = () => (new Promise((resolve, reject) => {
+  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+    .then((response) => response.json())
+    .then((data) => resolve(data.results))
+    .catch((erro) => reject(erro));
+})
+);
+const sectionItems = document.getElementsByClassName('items');
+searchComputer()
+  .then((lista) => {
+    lista.forEach((product) => {
+      const item = createProductItemElement({
+        sku: product.id,
+        name: product.title,
+        image: product.thumbnail,
+      });
+      sectionItems[0].appendChild(item);
+    });
+  });
 
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
@@ -26,18 +44,22 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+//  function getSkuFromProductItem(item) {
+//  return item.querySelector('span.item__sku').innerText;
+//  }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+//  function cartItemClickListener(event) {
+//  }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+//  function createCartItemElement({ sku, name, salePrice }) {
+//  const li = document.createElement('li');
+//  li.className = 'cart__item';
+//  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//  li.addEventListener('click', cartItemClickListener);
+//  return li;
+//  }
+
+window.onload = function onload() { 
+  searchComputer();
+  createProductItemElement();
+};
