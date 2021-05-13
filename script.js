298b1025b46d15237ec2d248028c7c69e2f50035
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2021-05-13 20:33:41 -0300

    Commit pra previnir de perder o progresso caso meu PC exploda

--- script.js
+++ script.js
@@ -14,6 +14,16 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+// function createCartItemElement({ id, title, price }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${id} | NAME: ${title} | PRICE: $${price}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
+
+const testFunction = (event) => console.log(event.target.parentNode.firstChild.innerText);
+
 function createProductItemElement({ id, title, thumbnail }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -21,19 +31,23 @@ function createProductItemElement({ id, title, thumbnail }) {
   section.appendChild(createCustomElement('span', 'item__sku', id));
   section.appendChild(createCustomElement('span', 'item__title', title));
   section.appendChild(createProductImageElement(thumbnail));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  const newButton = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  newButton.addEventListener('click', testFunction);
+  section.appendChild(newButton);
 
   return section;
 }
+
+// Essa função vai servir para fazer a requisição ao endpoint do termo pesquisado e em seguida acessar todos os termos retornados colocando como filhos do container para os Itens.
 
 const fetchItem = async (searchTerm) => {
   const itemsContainer = document.querySelector('.items');
   const searchResult = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${searchTerm}`)
   .then((response) => response.json())
-  .then((data) => data.results);
+  .then((data) => data.results)
+  .catch((error) => alert(`Erro na requisição: ${error}`));
 
   searchResult.forEach((product) => itemsContainer.appendChild(createProductItemElement(product)));
-  // itemsContainer.appendChild(createProductItemElement(searchResult));
 };
 
 fetchItem('computador');
@@ -44,12 +58,4 @@ fetchItem('computador');
 
 // function cartItemClickListener(event) {
 //   // coloque seu código aqui
-// }
-
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
 // }
