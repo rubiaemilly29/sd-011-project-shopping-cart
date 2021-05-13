795abec9721c39aa882c8c96cc12e34a04ab51e0
Hamander <1hamander@gmail.com>
2021-05-13 19:03:24 -0300

    Requisito 0%

--- script.js
+++ script.js
@@ -27,15 +27,15 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+}
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  //li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', cartItemClickListener);
   return li;
 }
 
@@ -71,6 +71,6 @@ function getProducts(query) {
 }
 getProducts('computador');
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
