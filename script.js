37459e4b032afc1d3cd24a78cbf83f33aabfc11f
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-05-13 17:48:51 -0300

    implementa requisito 3

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui 
+  cart.removeChild(event.target);
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
