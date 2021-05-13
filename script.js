d766795554f8ad7d87f87dbaed4af8059ae5c2ee
Gabriela Feijo <feijo.gabriela@gmail.com>
2021-05-13 19:28:56 -0300

    Adiciona requisito 1

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -14,7 +12,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -41,3 +39,20 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+const myPromise = () => new Promise(() => {
+  const myObject = {
+    method: 'GET',
+    headers: { Accept: 'application/json' },
+  };
+  const getItems = document.querySelector('.items');
+
+  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador', myObject)
+    .then((response) => response.json())
+    .then((elements) => elements.results
+      .forEach((item) => getItems.appendChild(createProductItemElement(item)))
+    .catch((error) => error));
+});
+myPromise();
+
+window.onload = function onload() { };
