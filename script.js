9dfefa8409233d24b85a871df3b06641fdf51dd5
marcoswarmling <marcoswarmling15@gmail.com>
2021-05-13 17:28:16 -0300

    pfv npm funciona

--- script.js
+++ script.js
@@ -30,8 +30,8 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-//function cartItemClickListener(event) {
-  // coloque seu código aqui
+function cartItemClickListener(event) {
+   
 }
 
 
