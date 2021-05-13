42dcc728ed7dc44e80a0b1dc2f932d7bfa6238b2
Daniel Leon <daniel.radialista@gmail.com>
2021-05-13 17:41:44 -0300

    requisitos 2 e 3, prontos

--- script.js
+++ script.js
@@ -12,24 +12,12 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-  return section;
-}
-
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  event.target.parentNode.removeChild(event.target); // requisito 3
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -38,6 +26,39 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
+}
+
+function localStorageSave() {
+  localStorage.setItem('carrinho', document.querySelector('.cart__items').innerHTML);
+} // carrinho é a key do localStorageSave, função usada para armazenar os itens no carrinho de compras
+
+function addProduct(event) { // requisito 2
+  const product = event.target.parentNode;
+  const idItem = getSkuFromProductItem(product);
+  fetch(`https://api.mercadolibre.com/items/${idItem}`)
+    .then((resolve) => resolve.json())
+    .then((data) => {
+      const item = {
+        sku: data.id,
+        name: data.title,
+        salePrice: data.price,
+      };
+      const itemSelect = document.querySelector('.cart__items');
+      itemSelect.appendChild(createCartItemElement(item));
+      localStorageSave();
+    });
+}
+
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'))
+    .addEventListener('click', addProduct); // requisito 2
+  return section;
 }
 
 const loading = () => { // req. 7 - criando função loading
