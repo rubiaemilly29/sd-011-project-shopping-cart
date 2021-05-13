fec6560c38b0fd230cee1c3f58f14fa84e935f8d
Marcelo Araujo <almeidadearaujomarcelo@gmail.com>
2021-05-13 17:29:13 -0300

    Requisito 3

--- script.js
+++ script.js
@@ -24,38 +24,40 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+  document.getElementsByTagName('ol')[0].removeChild(event.target);
+}
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  // li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', cartItemClickListener);
   return li;
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
+function appendProductToCart(event) {
   const product = event.target.parentElement;
-  fetch(`https://api.mercadolibre.com/items/${product.firstChild.innerText}`)
-    .then((response) => response.json())
-    .then((data) => {
-      document.getElementsByTagName('ol')[0].appendChild(createCartItemElement(
-        ({ sku: data.id,
-          name: data.title,
-          salePrice: data.price }),
-      ));
-    });
+  fetch(`https://api.mercadolibre.com/items/${getSkuFromProductItem(product)}`)
+  .then((response) => response.json())
+  .then((data) => {
+    document.getElementsByTagName('ol')[0].appendChild(createCartItemElement(
+      ({ sku: data.id,
+        name: data.title,
+        salePrice: data.price }),
+    ));
+  });
 }
 
 window.onload = function onload() {
   fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
     .then((response) => response.json())
     .then((data) => { 
-      // console.log(data);
-      // console.log(typeof data);
       for (let index = 0; index < data.paging.limit; index += 1) {
         document.getElementsByClassName('items')[0].appendChild(createProductItemElement(
           ({ sku: data.results[index].id,
@@ -64,7 +66,5 @@ window.onload = function onload() {
         ));
       }
     });
-  // console.log(fetchResult);
-  // createProductItemElement(fetchResult);
-  document.getElementsByClassName('items')[0].addEventListener('click', cartItemClickListener);
+  document.getElementsByClassName('items')[0].addEventListener('click', appendProductToCart);
 };
