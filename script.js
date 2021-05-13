31610dda992bd4d79f3f24e81cf37c4213e39c99
Diego Ribeiro <dihrm2001@gmail.com>
2021-05-13 16:56:57 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -28,8 +28,8 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
+async function cartItemClickListener(event) {
+  // wait a sec
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -40,19 +40,46 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-const showProducts = async () => {
-  const param = { headers: { Accept: 'application/json' } };
-  const parent = document.querySelector('.items');
-  await fetch('https://api.mercadolibre.com/sites/MLB/search?q=$computador', param)
+const addItemToCart = (sku) => {
+  const parent = document.querySelector('.cart__items');
+  const url = `https://api.mercadolibre.com/items/${sku}`;
+  const format = { headers: { Accept: 'application/json' } };
+  fetch(url, format)
     .then((response) => {
       response.json()
         .then((json) => {
-          json.results.forEach((item) => {
-            const { id: sku, title: name, thumbnail: image } = item;
-            const product = createProductItemElement({ sku, name, image });
-            parent.appendChild(product);
-          });
+          const { title: name, price: salePrice } = json;
+          const item = createCartItemElement({ sku, name, salePrice });
+          parent.appendChild(item);
         });
+    });
+};
+
+function addButtonEvent(item) {
+  const button = item.querySelector('.item__add');
+  button.addEventListener('click', (event) => {
+    const itemId = getSkuFromProductItem(event.target.parentNode);
+    addItemToCart(itemId);
+  });
+}
+
+const showProducts = async () => {
+  const parent = document.querySelector('.items');
+
+  const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  const format = { headers: { Accept: 'application/json' } };
+
+  await fetch(url, format)
+    .then((response) => {
+      response.json()
+      .then((json) => {
+        json.results.forEach((item) => {
+          const { id: sku, title: name, thumbnail: image } = item;
+          const product = createProductItemElement({ sku, name, image });
+          parent.appendChild(product);
+          addButtonEvent(product);
+        });
+      });
     });
 };
 
