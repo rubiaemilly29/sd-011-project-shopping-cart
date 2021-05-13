691a3255d18174f546876c3e2b82d118fb88a71e
Víctor Faria <vitilevi@hotmail.com>
2021-05-13 17:46:11 -0300

    Feito requisito 1

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-window.onload = function onload() { };
+const fetchApi = () => {
+  const endPoint = "https://api.mercadolibre.com/sites/MLB/search?q=computador";
+  const param = { headers: { Accept: 'application/json' } };
+  return new Promise((resolve, reject) => {
+  fetch(endPoint, param)
+  .then(response => resolve(response.json()))
+  .catch(error => reject(error));
+  })
+}
+
+const reduceResponse = (acc, curr) => {
+  acc.id = curr.id;
+  acc.title = curr.title;
+  acc.thumbnail = curr.thumbnail;
+  return acc
+}
+
+const formatApiResults = (resp) => {
+  const values = Object.values(resp);
+  const control = values.map(el => ({
+    sku: el.id, 
+    name: el.title,
+    image: el.thumbnail 
+  }));
+  return control;
+}
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -17,12 +42,12 @@ function createCustomElement(element, className, innerText) {
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
+  
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+  
   return section;
 }
 
@@ -41,3 +66,14 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+const getAndCreateElements = () => {
+  const items = document.querySelector('.items');
+  fetchApi()
+  .then(response => formatApiResults(response.results))
+  .then(formated => formated.forEach((el) => items.appendChild(createProductItemElement(el))));
+}
+
+window.onload = function onload() {
+  getAndCreateElements()
+};
