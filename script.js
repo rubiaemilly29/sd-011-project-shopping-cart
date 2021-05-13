e425cb7813bd1d82e44f83fed5019cf4479f2799
Diego Ribeiro <dihrm2001@gmail.com>
2021-05-13 17:00:38 -0300

    Requisito 3

--- script.js
+++ script.js
@@ -28,15 +28,15 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-// async function cartItemClickListener(event) {
-//   // wait a sec
-// }
+async function cartItemClickListener(event) {
+  event.target.remove();
+}
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  // li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', cartItemClickListener);
   return li;
 }
 
