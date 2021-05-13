6c6a9f0e0dca34075bd5db8004719e929a2b1ecc
jhenriquemargrafm <jh.margraf@gmail.com>
2021-05-13 17:38:13 -0300

    Requisito 2:Criaçao itens do carrinho, lidando com objetos e atributos

--- script.js
+++ script.js
@@ -12,15 +12,27 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+// Requisito 2 - Cria o item no carrinho com o sku, nome e preço
+function createCartItemElement({ sku, name, price }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${price}`;
+  // Requisito 2 - Criação do item no Carrinho
+  const shoppingCartItens = document.querySelector('.cart__items');
+  shoppingCartItens.appendChild(li);
+  return li;
+}
+
+// Requisito 2 - Pegar do objeto recebido, os parâmetros de cada um dos itens: SKU, Name, Image e Price serão usados
+function createProductItemElement({ id: sku, title: name, thumbnail: image, price }) {
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'))
+  // Colocar o AddEventListener para criar o item no carrinho com os atributos pedidos  
+  .addEventListener('click', () => createCartItemElement({ sku, name, price }));
   return section;
 }
 
@@ -29,31 +41,30 @@ function createProductItemElement({ sku, name, image }) {
 // }
 
 // function cartItemClickListener(event) {
-//  //// coloque seu código aqui
-// }
-
-// function createCartItemElement({ sku, name, salePrice }) {
-//  const li = document.createElement('li');
-//  li.className = 'cart__item';
-//  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//  li.addEventListener('click', cartItemClickListener);
-//  return li;
+  // coloque seu código aqui
 // }
 
 // Requisito 1 - Puxar a lista por API e criar item dentro da section
-const fetchTheAPI = () => {
-  fetch('"https://api.mercadolibre.com/sites/MLB/search?q=$computador')
-  .then((response) => {
-    response.json().then((data) => {
-      const productList = data.results;
-      productList.forEach((product) => { 
-        const section = document.querySelector('.items');
-        section.appendChild(createProductItemElement(product));
-      });
-    });
-  });
- };
+const fetchTheAPI = () => (
+  new Promise((resolve, reject) => {
+    fetch('https://api.mercadolibre.com/sites/MLB/search?q=$computador')
+      .then((response) => {
+        response.json()
+        .then((data) => {
+          console.log(data);
+          const productList = data.results;
+          productList.forEach((product) => {
+            const sectionItens = document.querySelector('.items');
+            sectionItens.appendChild(createProductItemElement(product));
+            resolve();
+          });
+          })
+        .catch(() => reject());
+      })
+      .catch(() => reject());
+  })
+);
 
- window.onload = function onload() { 
+window.onload = function onload() { 
   fetchTheAPI();
 };
