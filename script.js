0851c7369bfec13206d3245dd347e26bfb700a43
Arthur Hermann <arthurhermannc@gmail.com>
2021-05-14 20:14:50 -0300

    comitao bomba req 4, 5 e 6

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
-  img.src = imageSource;
+  img.src = imageSource; 
   return img;
 }
 
@@ -16,19 +16,44 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
+function createTotal() {
+  const list = document.querySelectorAll('.cart__item');
+  let sum = 0;
+    for (let index = 0; index < list.length; index += 1) {
+      const valor = list[index].innerHTML.split('$')[1];
+      sum += Number(valor);
+    }
+  const totalPrice = document.querySelector('.total-price');
+  totalPrice.innerHTML = sum;
+}
+
+function clearCart() {
+  const li = document.querySelectorAll('.cart__item');
+  const ol = document.querySelector('ol');
+  console.log(li);
+  for (let index = 0; index < li.length; index += 1) {
+    ol.removeChild(li[index]);
+  }
+  createTotal();
+}
+
+function cartItemClickListener(event, counter) {
   const local = event.target;
   document.querySelector('.cart__items').removeChild(local);
+  localStorage.removeItem(`item${counter}`);
+  createTotal();
 }
 
 function createCartItemElement({ sku, name, price }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${price}`;
-  li.addEventListener('click', (event) => cartItemClickListener(event));
   const cartList = document.querySelector('.cart__items');
+  const counter = cartList.childElementCount;
+  localStorage.setItem(`item${counter}`, `${sku}|${name}|${price}`);
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${price}`;
+  li.addEventListener('click', (event) => cartItemClickListener(event, counter));
   cartList.appendChild(li);
+  createTotal();
   return li;
 }
 
@@ -50,15 +75,23 @@ const fetchProducts = (product) => {
     method: 'GET',
     headers: { Accept: 'application/json' },
   };
-  
   fetch(API_URL, myObject)
     .then((response) => response.json())
       .then((products) => products.results.forEach(({ id, title, thumbnail, price }) => {
         document.querySelector('.items')
         .appendChild(createProductItemElement({ sku: id, name: title, image: thumbnail, price }));
-      }));
+      }))
+      .then(() => {
+        for (let index = 0; index < localStorage.length; index += 1) {
+          const [sku, name, price] = localStorage.getItem(`item${index}`).split('|');
+          const saved = { sku, name, price };
+          createCartItemElement(saved);
+        }
+      });
 };
 
 window.onload = function onload() {
   fetchProducts('computador');
+  const cart = document.querySelector('.empty-cart');
+  cart.addEventListener('click', clearCart);
 };
