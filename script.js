041cd046579adc0a761d093f2c22d1e0ac4f5bd1
Alan Gonçalves <vekio.soft@gmail.com>
2021-05-14 11:40:06 -0300

    requsito 2

--- script.js
+++ script.js
@@ -12,21 +12,6 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  const getItems = document.querySelector('.items');
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-  
-  getItems.appendChild(section);
-  return section;
-}
-
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
@@ -35,13 +20,35 @@ function cartItemClickListener(event) {
   // coloque seu código aqui
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ sku, name, price }) {
   const li = document.createElement('li');
+  const ol = document.querySelector('.cart__items');
   li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${price}`;
   li.addEventListener('click', cartItemClickListener);
+  ol.appendChild(li);
   return li;
 }
+
+// 2. Adicione o produto ao carrinho de compras
+
+function createProductItemElement({ id: sku, title: name, thumbnail: image, price }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  const getItems = document.querySelector('.items');
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'))
+  .addEventListener('click', () => createCartItemElement({ sku, name, price }));
+  
+  getItems.appendChild(section);
+  return section;
+}
+
+// 1. Crie uma lista de produtos
 
 function chargePage() {
   fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
