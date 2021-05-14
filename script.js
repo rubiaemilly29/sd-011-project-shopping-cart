0b97fe655b0ca281f700d91d63257960a81faf1a
IsleneGomes <islenegomes@gmail.com>
2021-05-14 20:24:36 -0300

    Requisito 4

--- script.js
+++ script.js
@@ -28,17 +28,17 @@ function createProductItemElement({ sku, name, image }) {
 // getSkuFromProductItem();
 
 const sumPrices = () => {
-const totalPrice = document.querySelector('.total-price');
-const lis = [...document.querySelectorAll('.cart__item')];
-totalPrice.innerText = 0;
-const somaLis = lis.reduce((acc, curr) => acc + Number(curr.innerText.split('PRICE: $')[1]), 0);
-totalPrice.innerText = somaLis;
+  const totalPrice = document.querySelector('.total-price');
+  const lis = [...document.querySelectorAll('.cart__item')];
+  totalPrice.innerText = 0;
+  const somaLis = lis.reduce((acc, curr) => acc + Number(curr.innerText.split('PRICE: $')[1]), 0);
+  totalPrice.innerText = somaLis;
 };
 
 function cartItemClickListener(event, contador) {
   localStorage.removeItem(`items ${contador}`);
   event.target.remove();
-  return sumPrices();
+  sumPrices();
 }
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }, contador) {
@@ -50,35 +50,45 @@ function createCartItemElement({ id: sku, title: name, price: salePrice }, conta
 }
 
 const getItens = () => {
-const buttonCart = document.querySelectorAll('.item__add');
-const itensCart = document.querySelector('.cart__items');
-buttonCart.forEach((elemento) => elemento.addEventListener('click', () => {
-const id = elemento.parentElement.firstChild.innerText;
-const contador = itensCart.childElementCount;
- fetch(`https://api.mercadolibre.com/items/${id}`)
-  .then((response) => response.json())
-  .then((data) => itensCart.appendChild(createCartItemElement(data, contador)))
-  .then(() => sumPrices());
-  localStorage.setItem(`items ${contador}`, id);
+  const buttonCart = document.querySelectorAll('.item__add');
+  const itensCart = document.querySelector('.cart__items');
+  buttonCart.forEach((elemento) => elemento.addEventListener('click', () => {
+    const id = elemento.parentElement.firstChild.innerText;
+    const contador = itensCart.childElementCount;
+    fetch(`https://api.mercadolibre.com/items/${id}`)
+      .then((response) => response.json())
+      .then((data) => itensCart.appendChild(createCartItemElement(data, contador)))
+      .then(() => sumPrices());
+    localStorage.setItem(`items ${contador}`, id);
   }));
 };
 
-const listOfProducts = () => {
-const api = `https://api.mercadolibre.com/sites/MLB/search?q=${'computador'}`;
-const myObject = {
-  method: 'GET',
-  headers: { Accept: 'application/json' },
+const recuperaLocalStorage = () => {
+  const itensCart = document.querySelector('.cart__items');
+  for (let index = 0; index < localStorage.length; index += 1) {
+    const local = localStorage.getItem(`items ${index}`);
+    fetch(`https://api.mercadolibre.com/items/${local}`)
+      .then((response) => response.json())
+      .then((data) => itensCart.appendChild(createCartItemElement(data, index)))
+      .then(() => sumPrices());
+  }
 };
+
+const listOfProducts = () => {
+  const api = `https://api.mercadolibre.com/sites/MLB/search?q=${'computador'}`;
+  const myObject = {
+    method: 'GET',
+    headers: { Accept: 'application/json' },
+  };
   fetch(api, myObject)
-  .then((response) => {
-    response.json()
-    .then((data) => data.results
-    .forEach(({ id, title, thumbnail }) => {
-      const sectionItem = document.querySelector('.items');
-      const objList = createProductItemElement({ sku: id, name: title, image: thumbnail });
-      sectionItem.appendChild(objList);
-    })).then(() => getItens()).then(() => sumPrices());
-  });
+    .then((response) => {
+      response.json().then((data) => data.results
+        .forEach(({ id, title, thumbnail }) => {
+          const sectionItem = document.querySelector('.items');
+          const objList = createProductItemElement({ sku: id, name: title, image: thumbnail });
+          sectionItem.appendChild(objList);
+        })).then(() => getItens()).then(() => recuperaLocalStorage());
+    });
 };
 
 const esvaziar = () => {
@@ -90,6 +100,6 @@ const esvaziar = () => {
 };
 
 window.onload = () => {
-listOfProducts();
-esvaziar();
+  listOfProducts();
+  esvaziar();
 };
