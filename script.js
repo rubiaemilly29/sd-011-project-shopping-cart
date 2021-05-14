759087909f6dead77291bf7620a009cd3c5ffc41
camiloLelis <lafirmafuerte@gmail.com>
2021-05-14 16:43:00 -0300

    requisito-1-tentativa-1

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -17,27 +15,48 @@ function createCustomElement(element, className, innerText) {
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
+  
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+  
   return section;
 }
 
-function getSkuFromProductItem(item) {
+/* function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
+} */
 
-function cartItemClickListener(event) {
+/* function cartItemClickListener(event) {
   // coloque seu código aqui
-}
+} */
 
-function createCartItemElement({ sku, name, salePrice }) {
+/* function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-}
+} */
+const dataMl = async () => {
+  const fetchMLivre = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  const cleanJson = fetchMLivre.json();
+  return cleanJson;
+};
+const createHome = async () => {
+  const ol = document.querySelector('section.items');
+  try {
+    const datas = await dataMl();
+    await datas.results.forEach(({ id, title, thumbnail }) => {
+      const forAppend = createProductItemElement({ sku: id, name: title, image: thumbnail });
+      ol.appendChild(forAppend);
+    });
+  } catch (error) {
+    return error;
+  }
+};
+
+window.onload = function onload() { 
+  createHome();
+};
