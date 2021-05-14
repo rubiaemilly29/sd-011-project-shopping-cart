da1c8939feac2e3a8951692e7a3cb94c023dae68
Luan Xavier <luantenorio3@gmail.com>
2021-05-14 18:31:30 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -12,20 +12,6 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-  return section;
-}
-
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
 function cartItemClickListener(event) {
   // coloque seu código aqui
 }
@@ -36,6 +22,34 @@ function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
+}
+
+async function getItemByID(id) {
+  const response = await fetch(`https://api.mercadolibre.com/items/${id}`);
+  const itemData = await response.json();
+  return itemData;
+}
+
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+
+async function addItemToCart(event) {
+  const cartList = document.querySelector('.cart__items');
+  const id = await getSkuFromProductItem(event.target.parentElement);
+  const item = await getItemByID(id);
+  cartList.appendChild(createCartItemElement(item));
+}
+
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'))
+  .addEventListener('click', addItemToCart);
+  return section;
 }
 
 function render() {
