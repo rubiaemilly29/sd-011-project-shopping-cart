5f29e0853514a77c37a8cba5666b981e9004deab
Vitor <vitorguima@gmail.com>
2021-05-13 21:14:32 -0300

    requisito 2 finalizado

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+const paramJSON = { headers: { Accept: 'application/json' } };
 function getProductList() {
   const APIurl = 'https://api.mercadolibre.com/sites/MLB/search?q=$computador';
-  return fetch(APIurl)
+  return fetch(APIurl, paramJSON)
     .then((response) => response.json())
     .then((json) => (json.results
       .map(({ id, title, thumbnail }) => ({ sku: id, name: title, image: thumbnail }))));
@@ -37,17 +38,12 @@ function loopCreateElement(array) {
   array.forEach((element) => getItemsSection.appendChild(createProductItemElement(element)));
 }
 
-async function execute() {
-  const productList = await getProductList();
-  loopCreateElement(productList);
-}
-
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  // 
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -56,6 +52,32 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
+}
+
+function insertItemToCart(object) {
+  const cartContainer = document.querySelector('.cart__items');
+  cartContainer.appendChild(createCartItemElement(object));
+}
+
+function fetchSelectedItem(param) {
+  return fetch(`https://api.mercadolibre.com/items/${param}`, paramJSON)
+  .then((response) => response.json())
+  .then(({ id, title, price }) => ({ sku: id, name: title, salePrice: price }))
+  .then((object) => insertItemToCart(object));
+}
+
+function getElement() {
+  const getItemsBoard = document.querySelectorAll('.item');
+  getItemsBoard.forEach((element) => element.lastChild.addEventListener('click', (event) => {
+    console.log(event.target.parentNode.firstChild.innerText);
+    return fetchSelectedItem(event.target.parentNode.firstChild.innerText);
+  }));
+}
+
+async function execute() {
+  const productList = await getProductList();
+  await loopCreateElement(productList);
+  await getElement();
 }
 
 window.onload = function onload() {
