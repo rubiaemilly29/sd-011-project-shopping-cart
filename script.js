8fe0a8d80b5bbf85398b04a3971a6cb021cae379
marsleite <marsleite@gmail.com>
2021-05-14 17:21:41 -0300

    requisito 01 - done

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -14,10 +12,13 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
+  const items = document.querySelector('.items');
+  items.appendChild(section);
+  
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
@@ -41,3 +42,11 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+const fetchMl = () => {
+  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+    .then((res) => res.json())
+    .then((data) => data.results.map((product) => createProductItemElement(product)));
+};
+
+window.onload = () => fetchMl();
