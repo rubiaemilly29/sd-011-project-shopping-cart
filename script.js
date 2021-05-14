a33fe21f66d52fd9ea5fd18ecc3decf47aac0cf7
Bruno Duarte <brunoarduarte@gmail.com>
2021-05-14 20:06:38 -0300

    Ajusre Requisito 1

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-window.onload = function onload() { };
-
+// cria a imagem dos produtos
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -7,6 +6,7 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
+// configura o elemento criado
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
@@ -14,26 +14,32 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+// cria uma lista de produtos
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
+  const sectionItem = document.querySelector('.items');
 
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
+  sectionItem.appendChild(section);
   return section;
 }
 
+// captura o sku
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
+// remove o item do carrinho de compras
 function cartItemClickListener(event) {
-  
+  // coloque seu código aqui
 }
 
+// adiciona o produto ao carrinho de compras
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
@@ -41,3 +47,14 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+window.onload = function onload() {
+  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+      .then((response) => response.json())
+      .then((json) => {
+        json.results.forEach(({ id, title, thumbnail }) => {
+          createProductItemElement({ sku: id, name: title, image: thumbnail });
+          });
+      })
+      .catch((e) => window.alert(e));
+  };
