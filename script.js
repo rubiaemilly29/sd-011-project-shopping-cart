0eddbfdf169862a06bcafc1e2f4d6e80fe0a832d
Arthur Hermann <arthurhermannc@gmail.com>
2021-05-14 13:17:07 -0300

    requisito 2 e 3

--- script.js
+++ script.js
@@ -12,32 +12,36 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-  return section;
-}
-
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
+  const local = event.target;
+  document.querySelector('.cart__items').removeChild(local);
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ sku, name, price }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${price}`;
+  li.addEventListener('click', (event) => cartItemClickListener(event));
+  const cartList = document.querySelector('.cart__items');
+  cartList.appendChild(li);
   return li;
+}
+
+function createProductItemElement({ sku, name, image, price }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'))
+  .addEventListener('click', () => createCartItemElement({ sku, name, price }));
+
+  return section;
 }
 
 const fetchProducts = (product) => {
@@ -49,9 +53,9 @@ const fetchProducts = (product) => {
   
   fetch(API_URL, myObject)
     .then((response) => response.json())
-      .then((products) => products.results.forEach(({ id, title, thumbnail }) => {
+      .then((products) => products.results.forEach(({ id, title, thumbnail, price }) => {
         document.querySelector('.items')
-        .appendChild(createProductItemElement({ sku: id, name: title, image: thumbnail }));
+        .appendChild(createProductItemElement({ sku: id, name: title, image: thumbnail, price }));
       }));
 };
 
