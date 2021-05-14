531c5262a0a4d0fe0fb8797fd99dce19b2c053be
Tulio Sploradori <tuliospl@gmail.com>
2021-05-14 11:28:18 -0300

    conclusao do item 3

--- script.js
+++ script.js
@@ -28,7 +28,8 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) { 
+function cartItemClickListener(event) {
+  return event.target.remove();
 }
   
   function createCartItemElement({ sku, name, salePrice }) {
