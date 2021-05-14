bb0844c05a32214a68f5fb0b2c091d18943a9862
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-05-14 19:22:45 -0300

    Requisito 5 e 7 realizados

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const actualItems = document.querySelector('.cart__items');
+const totalPrice = document.querySelector('.total-price');
+const container = document.querySelector('.items');
+
+const updateTotalPrice = (price) => {
+  totalPrice.innerText = (
+    parseFloat(totalPrice.innerText) + parseFloat(price)
+  );
+};
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -21,7 +29,9 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(
+    createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'),
+  );
 
   return section;
 }
@@ -31,8 +41,10 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
+  updateTotalPrice(-event.target.innerText.split('$')[1]);
   event.target.remove();
   localStorage.setItem('actualCart', actualItems.innerHTML);
+  localStorage.setItem('actualPrice', totalPrice.innerText);
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -43,41 +55,65 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
+const showLoading = () => {
+  const loading = document.createElement('p');
+  loading.className = 'loading';
+  loading.innerText = 'Processando Requisição';
+  container.appendChild(loading);
+};
+
+const closeLoading = () => {
+  const loading = document.querySelector('.loading');
+  loading.remove();
+};
+
 const createItems = () => {
+  showLoading();
   const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=$computador';
   const params = { method: 'GET', headers: { Accept: 'application/json' } };
   const itemsList = document.querySelector('.items');
   return fetch(API_URL, params)
     .then((response) => response.json())
-    .then((json) => json.results
-      .forEach((element) => itemsList
-        .appendChild(createProductItemElement(
-          { sku: element.id, name: element.title, image: element.thumbnail },
-))));
+    .then((json) => {
+      json.results.forEach((element) =>
+        itemsList.appendChild(
+          createProductItemElement({
+            sku: element.id,
+            name: element.title,
+            image: element.thumbnail,
+          }),
+        )); 
+})
+      .then(closeLoading);
 };
 
 const sendItemtoCart = () => {
   const addItemToCart = document.querySelectorAll('.item__add');
   const params = { method: 'GET', headers: { Accept: 'application/json' } };
   const cartItems = document.querySelector('.cart__items');
-  addItemToCart.forEach((element) => 
-    element.addEventListener('click', () => 
+  addItemToCart.forEach((element) =>
+    element.addEventListener('click', () =>
       fetch(
-        `https://api.mercadolibre.com/items/${element.parentNode.children[0].innerText}`, params,
+        `https://api.mercadolibre.com/items/${element.parentNode.children[0].innerText}`,
+        params,
       )
         .then((response) => response.json())
-          .then((json) => cartItems
-            .appendChild(createCartItemElement(
-              { sku: json.id, name: json.title, salePrice: json.price },
-            ))).then(() => localStorage
-                 .setItem('actualCart', actualItems.innerHTML))));
+        .then((json) => {
+          cartItems.appendChild(
+            createCartItemElement({ sku: json.id, name: json.title, salePrice: json.price }),
+          );
+          updateTotalPrice(json.price);
+        })
+        .then(() => localStorage.setItem('actualCart', actualItems.innerHTML))
+        .then(() => localStorage.setItem('actualPrice', totalPrice.innerText))));
 };
 
 const retrieveCart = async () => {
   if (localStorage.actualCart) {
     actualItems.innerHTML = localStorage.getItem('actualCart');
     actualItems.addEventListener('click', cartItemClickListener);
-  } 
+    totalPrice.innerText = localStorage.getItem('actualPrice');
+  }
 };
 
 const emptyCart = () => {
@@ -93,4 +129,6 @@ const createStore = async () => {
   await sendItemtoCart();
 };
 
-window.onload = function onload() { createStore(); }; 
+window.onload = function onload() {
+  createStore();
+};
