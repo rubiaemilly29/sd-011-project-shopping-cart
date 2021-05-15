b8fed4989ec199f6d32ec3fedc14aeab7ac6201d
Michelle <mls.srbek@gmail.com>
2021-05-14 23:45:05 -0300

    req 2 eslint corrections

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ function getSkuFromProductItem(item) {
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
+  console.log(event);
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
