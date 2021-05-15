e7e94ca7f4c911f69dd538b8450fb69250c7c6e3
Bruno Duarte <brunoarduarte@gmail.com>
2021-05-15 20:59:18 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -16,30 +16,37 @@ function createCustomElement(element, className, innerText) {
 }
 
 // remove o item do carrinho de compras
-function cartItemClickListener(event) {
+function cartItemClickListener(price, event) {
   const eventTg = event.target;
   const cartOl = document.querySelector(cartItems);
-
+  const spanTotal = document.querySelector('.total-price');
+  spanTotal.innerHTML = parseFloat(Number(spanTotal.innerHTML) - Number(price));
   eventTg.remove();
   localStorage.setItem('items', cartOl.innerHTML);
 }
 
+function sumOfPrices(price) {
+  const spanTotal = document.querySelector('.total-price');
+  spanTotal.innerHTML = parseFloat(Number(spanTotal.innerHTML) + Number(price));
+}
+
 // adiciona o produto ao carrinho de compras
-function createCartItemElement({ sku: id, name: title, salePrice: price }) {
+function createCartItemElement({ sku: id, name: title, price }) {
   const li = document.createElement('li');
   const cartOl = document.querySelector(cartItems);
 
   li.className = 'cart__item';
   li.innerText = `SKU: ${id} | NAME: ${title} | PRICE: $${price}`;
-  li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', (event) => cartItemClickListener(price, event));
 
   cartOl.appendChild(li);
   localStorage.setItem('items', cartOl.innerHTML);
+  sumOfPrices(price);
   return li;
 }
 
 // cria uma lista de produtos
-function createProductItemElement({ sku, name, image, salePrice }) {
+function createProductItemElement({ sku, name, image, price }) {
   const section = document.createElement('section');
   section.className = 'item';
   const sectionItem = document.querySelector('.items');
@@ -48,7 +55,9 @@ function createProductItemElement({ sku, name, image, salePrice }) {
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'))
-    .addEventListener('click', () => createCartItemElement({ sku, name, salePrice }));
+    .addEventListener('click', () => {
+      createCartItemElement({ sku, name, price });
+    });
 
   sectionItem.appendChild(section);
   return section;
@@ -62,9 +71,9 @@ function getSkuFromProductItem(item) {
 function getLocalStorage() {
   const getStorage = localStorage.getItem('items');
   const cartOl = document.querySelector(cartItems);
-  
+
   cartOl.innerHTML = getStorage;
-  
+
   const cartLi = document.querySelectorAll('.cart__item');
   cartLi.forEach((elements) => elements.addEventListener('click', cartItemClickListener));
 }
@@ -74,10 +83,10 @@ window.onload = function onload() {
     .then((response) => response.json())
     .then((json) => {
       json.results.forEach(({ id, title, thumbnail, price }) => {
-        createProductItemElement({ sku: id, name: title, image: thumbnail, salePrice: price });
+        createProductItemElement({ sku: id, name: title, image: thumbnail, price });
       });
     })
     .catch((e) => window.alert(e));
 
-    getLocalStorage();
+  getLocalStorage();
 };
