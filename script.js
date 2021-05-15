1ca8f30b44030222784712d87e283ea9ff0b4776
Reginara Gomes <refagomes@gmail.com>
2021-05-15 13:19:52 -0300

    Corrige linter requisito  6

--- script.js
+++ script.js
@@ -24,9 +24,9 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
 const deleteCart = () => {
   const emptyButton = document.querySelector('.empty-cart');
