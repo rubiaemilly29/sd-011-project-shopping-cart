9622b493ecac3187b2d63bf5e34152a56ddaa1fc
Maran <maranmorimoto@gmail.com>
2021-05-16 17:02:34 -0300

    Changing the way to build requests 1, 2 and 3, and request 4 done - fixing lint errors

--- script.js
+++ script.js
@@ -12,7 +12,24 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ id: sku, title: name, thumbnail: image,  price}) {
+function cartItemClickListener(event, items, count) {
+  event.target.remove();
+  localStorage.removeItem(`item${count}`);
+}
+
+function createCartItemElement({ sku, name, price }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${price}`;
+  const items = document.querySelector('.cart__items');
+  localStorage.setItem(`item${items.childElementCount}`, `${sku}|${name}|${price}`);
+  const count = items.childElementCount;
+  items.appendChild(li);
+  li.addEventListener('click', (event) => cartItemClickListener(event, items, count));
+  return li;
+}
+
+function createProductItemElement({ id: sku, title: name, thumbnail: image, price }) {
   const items = document.querySelector('.items');
   const section = document.createElement('section');
   section.className = 'item';  
@@ -20,7 +37,8 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image,  pri
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!')).addEventListener('click', () => createCartItemElement({sku, name, price}));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'))
+   .addEventListener('click', () => createCartItemElement({ sku, name, price }));
   items.appendChild(section);
 
   return section;
@@ -30,23 +48,6 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event, items, count,) {
-event.target.remove();
-localStorage.removeItem(`item${count}`)
-}
-
-function createCartItemElement({ sku, name, price}) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${price}`;
-  const items = document.querySelector('.cart__items');
-  localStorage.setItem(`item${items.childElementCount}`, `${sku}|${name}|${price}`);
-  const count = items.childElementCount;
-  items.appendChild(li)
-  li.addEventListener('click',(event) => cartItemClickListener(event, items, count, price));
-  return li;
-}
-
 const fetchAPI = (product = 'computador') => {
   fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`)
   .then((response) => response.json())
@@ -54,12 +55,12 @@ const fetchAPI = (product = 'computador') => {
       createProductItemElement(item))))
         // .then(() => addToCart())
           .then(() => {
-            for(let index = 0; index < localStorage.length; index += 1) {
+            for (let index = 0; index < localStorage.length; index += 1) {
               const [sku, name, price] = (localStorage.getItem(`item${index}`).split('|'));
-              const obj = {sku, name, price};
+              const obj = { sku, name, price };
               createCartItemElement(obj);
             }
-          })
+          });
 };
 
 window.onload = function onload() { 
