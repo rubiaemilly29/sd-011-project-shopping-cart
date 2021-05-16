62a49cf6a0777a1319f6c0411743cbcf50f5d597
Jessica Arruda <sicarruda@gmail.com>
2021-05-16 20:32:26 -0300

    Exercicio 6

--- script.js
+++ script.js
@@ -16,8 +16,41 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+// Totalmente denecessário essa complexidade no exercício.
+function makeTrybeHappy(price) {
+  const priceParts = price.toFixed(2).split('.');
+
+  const intPart = priceParts[0];
+  // Isso é uma regex. Que substitui todas as ocorrencias de 0 seguidas do final do texto por nada.
+  // https://stackoverflow.com/a/20439411
+  const floatPart = priceParts[1].replace(/0*$/, '');
+  // "" -> false
+  const floatPartWithDot = floatPart ? `.${floatPart}` : '';
+  
+  return `${intPart}${floatPartWithDot}`;
+}
+
+function totalPriceCart() {
+  const total = document.querySelector('#total-price');
+  let totalPrice = 0;
+  if (localStorage.length === 0) {
+    total.innerText = 0;
+    return;
+  }
+  for (let index = 0; index < localStorage.length; index += 1) {
+    const itemJson = localStorage.getItem(localStorage.key(index));
+    const item = JSON.parse(itemJson);
+    const floatPrice = item.price;
+    totalPrice += floatPrice;
+  }
+
+  total.innerText = makeTrybeHappy(totalPrice);
+}
+
 function cartItemClickListener(event) {
   const parent = document.querySelector(classParentOl);
+  localStorage.removeItem(event.target.classList[1]);
+  totalPriceCart();
   return parent.removeChild(event.target);
 }
 
@@ -28,9 +61,11 @@ function createItemLocalStorage(sku, name, salePrice) {
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   createItemLocalStorage(sku, name, salePrice);
+  totalPriceCart();
   const ol = document.querySelector(classParentOl);
   const li = document.createElement('li');
   li.className = 'cart__item';
+  li.classList.add(sku);
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   ol.appendChild(li);
   li.addEventListener('click', cartItemClickListener);
@@ -91,14 +126,15 @@ function emptyCart() {
   const parentOl = document.querySelector(classParentOl);
   children.forEach((child) => parentOl.removeChild(child));
   localStorage.clear();
+  totalPriceCart();
 }
 
 window.onload = function onload() {
   const emptyCartButton = document.querySelector('#empty-cart');
-  emptyCartButton.addEventListener('click', emptyCart);
   fetch(`${URL_API}?q=computador`)
     .then((response) => response.json())
     .then((responseJson) => responseJson.results)
     .then((results) => results.forEach((result) => createProductItemElement(result)));
   reloadCartFromLocalStorage();
+  emptyCartButton.addEventListener('click', emptyCart);
 };
