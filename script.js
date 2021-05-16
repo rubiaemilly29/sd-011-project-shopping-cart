78f7134dfe51cf2da603652cc2826570a18f85a1
lucasSCsantos <luc.cristovam10@gmail.com>
2021-05-16 01:05:30 -0300

    tst

--- script.js
+++ script.js
@@ -49,9 +49,9 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
 function cartItemClickListener(event) {
   if (event.target.className === 'cart__item') {
