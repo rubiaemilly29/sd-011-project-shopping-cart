d19d10ebe8ff9c06572d29378972f3cf00c63e54
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-05-16 14:55:24 -0300

    Requisito 4 feito

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-async function newCartItem(sku, cartItemsList) {
+async function newCartItem(sku, cartItemsList, savedCartItems, save = true) {
   const fetchParameter = {
     method: 'GET',
     'Content-Type': 'application/json',
@@ -62,18 +62,45 @@ async function newCartItem(sku, cartItemsList) {
         name: json.title,
         salePrice: json.price,
       };
+      if (save !== false) savedCartItems.push(itemAtributtes.sku);
+      localStorage.cartItems = JSON.stringify(savedCartItems);
       const cartItem = createCartItemElement(itemAtributtes);
       cartItemsList.appendChild(cartItem);
     });
 }
 
-function cartItemClickListener(event, items) {
+function removeCartItem(cartItem, cartItemsList, savedCartItems) {
+  cartItemsList.removeChild(cartItem);
+  const sku = cartItem.innerText.split(' ')[1];
+  savedCartItems.forEach((item, index) => {
+  let deletedItems = 0;
+    if (item === sku && deletedItems === 0) {
+      savedCartItems.splice(index, 1);
+      deletedItems += 1;
+    }
+  });
+  localStorage.cartItems = JSON.stringify(savedCartItems);
+}
+
+function cartItemClickListener(event, cartItemsList) {
+  const savedCartItems = JSON.parse(localStorage.getItem('cartItems'));
   if (event.target.className === 'item__add') {
     const sku = event.target.parentNode.firstChild.innerText;
-    newCartItem(sku, items[1]);
+    newCartItem(sku, cartItemsList, savedCartItems);
   }
   if (event.target.className === 'cart__item') {
-  items[1].removeChild(event.target);
+    removeCartItem(event.target, cartItemsList, savedCartItems);
+  }
+}
+
+function recoverCart(cartItemsList) {
+  if (localStorage.getItem('cartItems') !== null) {
+    const savedCartItems = JSON.parse(localStorage.getItem('cartItems'));
+    savedCartItems.forEach((cartItem) => {
+      newCartItem(cartItem, cartItemsList, savedCartItems, false);
+    });
+  } else {
+    localStorage.setItem('cartItems', JSON.stringify([]));
   }
 }
 
@@ -82,9 +109,10 @@ window.onload = function onload() {
   const itemsSection = document.querySelector('.items');
   const cartItemsList = document.querySelector('.cart__items');
   const items = [itemsSection, cartItemsList];
+  recoverCart(cartItemsList);
   items.forEach((item) => {
     item.addEventListener('click', (event) => {
-      cartItemClickListener(event, items);
+      cartItemClickListener(event, cartItemsList);
     });
   });
 };
