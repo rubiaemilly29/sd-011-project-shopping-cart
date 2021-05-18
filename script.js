d615c8d0b75d869e76066336536781d93045e6dc
Michel Pereira <michelbecauro@gmail.com>
2021-05-18 09:54:39 -0300

    feature/Requisito_1/Maybe this is finished

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-// const fetch = require('node-fetch');
+// const fetch = require('node-fetch'); // Isso parece não ser necessário.
 
-// function createProductImageElement(imageSource) {
-//   const img = document.createElement('img');
-//   img.className = 'item__image';
-//   img.src = imageSource;
-//   return img;
-// }
+async function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+}
 
-// function createCustomElement(element, className, innerText) {
-//   const e = document.createElement(element);
-//   e.className = className;
-//   e.innerText = innerText;
-//   return e;
-// }
+async function createCustomElement(element, className, innerText) {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+}
 
-// function createProductItemElement({ sku, name, image }) {
-//   const section = document.createElement('section');
-//   section.className = 'item';
+async function createProductItemElement(sku, name, image) { // Modificado aqui. Parâmetros estavam entre chavez
+  const section = document.createElement('section');
+  section.className = 'item';
 
-//   section.appendChild(createCustomElement('span', 'item__sku', sku));
-//   section.appendChild(createCustomElement('span', 'item__title', name));
-//   section.appendChild(createProductImageElement(image));
-//   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(await createCustomElement('span', 'item__sku', sku));
+  section.appendChild(await createCustomElement('span', 'item__title', name));
+  section.appendChild(await createProductImageElement(image));
+  section.appendChild(await createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
-//   return section;
-// }
+  return section;
+}
 
 // function getSkuFromProductItem(item) {
 //   return item.querySelector('span.item__sku').innerText;
@@ -44,36 +44,36 @@
 
 // Meu códido aqui ABAIXO também:
 
-async function getProductList (productType) {
-// return new Promise ((resolve, reject) => {
-
+async function getProductList (productType) { // Criei essa função como PROMISE
   if (productType === 'computador') {
     const param = { headers: { Accept: 'application/json' } };
-    return fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${productType}`, param)
+    return fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${productType}`, param) // Obtenção de dados da API
    .then((r) => {
-    //  console.log(r);
+      // ((r) => {
       r.json()
-      .then((resolve) => console.log(resolve));
+      .then((resolve) => { // Resposta da APi já transforamda em JSON
+        const produto = document.querySelector('.container .items');
+        resolve.results.forEach(async (element) => { // Filtra para percorrer apenas o array de objetos que são cada produto
 
-      // resolve();  
-    })
+          const dados = { // Filtro para obteção de somente os dados que se quer passar, de cada produto, para a função createProduvtItemElement()
+            id: element.id,
+            title: element.title,
+            thumbnail: element.thumbnail,
+          };
+          const item = await createProductItemElement(dados.id, dados.title, dados.thumbnail);
+          console.log(`Aqui embaixo o que a funcao retorna:`);
+          console.log(item);
+          produto.appendChild(item);
+        });          
+      }) // >> Fim do segundo THEN        
+    }) // >> Fim do primeiro THEN  
   
   }
-  throw new Error ('Pequisa só com a palavra "computador"')
-
-// })
-  
+  throw new Error ('Pequisa só com a palavra "computador"');
 }
 
 // :Meu códido aqui ACIMA também:
 
-
 window.onload = function onload() { 
-alert('Oi, blz?')
-  // const fetch = require('node-fetch');
 getProductList('computador');
-
-
-
-
 };
