73d01b72a1424379b064feeaa43f2ee5b2186cc9
fernando lasmar <f3rnandolasmar@gmail.com>
2021-05-19 15:40:48 -0300

    resolvendo conflitos

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-window.onload = function onload() { };
 const api = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
-
-function createProductImageElement(imageSource) {
-  const img = document.createElement('img');
-  img.className = 'item__image';
-  img.src = imageSource;
-  return img;
-}
+const itemsCart = '.cart__items';
 
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
@@ -15,13 +8,33 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+}
+
+function storage() {
+  const cartStorage = document.querySelector(itemsCart);
+  localStorage.setItem('shopCart', cartStorage.innerHTML);
+}
+
+// 3 Para remover o item do carrinho
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+  const delItem = document.querySelector(itemsCart);
+  delItem.removeChild(event.target);
+  storage();
+ }
+
+ function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
-  const cartItems = document.querySelector('.cart__items');  //
-  cartItems.appendChild(li); //
+  const cartItems = document.querySelector(itemsCart);
+  cartItems.appendChild(li);
   return li;
 }
 
@@ -32,10 +45,14 @@ const fetchApiCartItem = (id) => {
   fetch(API_URL, headers)
     .then((response) => response.json())
     .then((json) => {
-      const cartList = document.querySelector('.cart__items');
+      const cartList = document.querySelector(itemsCart);
       cartList.appendChild(createCartItemElement(json));
     });
 };
+
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;  
+}
 
 function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
@@ -69,15 +86,12 @@ const fetchApi = () => {
 };
 fetchApi();
 
-window.onload = function onnload() { fetchApi(); };
-
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;  
-}
-
-// 3 Para remover o item do carrinho
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-  const delItem = document.querySelector('.cart__items');
-  delItem.removeChild(event.target);
- }
+ window.onload = function onload() { 
+  fetchApi();
+  if (localStorage.shopCart) {
+    document.querySelector(itemsCart).innerHTML = localStorage.getItem('shopCart');
+    document.querySelectorAll('.cart__item').forEach((li) => {
+      li.addEventListener('click', cartItemClickListener);
+    });
+  }
+};
