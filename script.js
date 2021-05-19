097cdb136befcbf440b981581827063915b4f1d1
Julio Cesar <julio.cesarcj1402@gmail.com>
2021-05-19 13:28:26 -0300

    testando correção de erro

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+// get endPoint
 const endPoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
 
+// create the product image and sent it to create element 
 const createProductImageElement = (imageSource) => {
   const imgFeature = document.createElement('img');
   imgFeature.className = 'item__image';
@@ -7,6 +9,7 @@ const createProductImageElement = (imageSource) => {
   return imgFeature;
 };
 
+// shortCut to creat an element and aply a class and an information
 const createCustomElement = (element, className, innerText) => {
   const e = document.createElement(element);
   e.className = className;
@@ -14,11 +17,11 @@ const createCustomElement = (element, className, innerText) => {
   return e;
 };
 
+// create an element using de shortcut to make it automated
 const createProductItemElement = ({ id: sku, title: name, thumbnail: image }) => {
   const section = document.createElement('section');
   const sectionOfItems = document.querySelector('.items');
   section.className = 'item';
-  
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
@@ -28,43 +31,47 @@ const createProductItemElement = ({ id: sku, title: name, thumbnail: image }) =>
   return sectionOfItems;
 };
 
-const getItemInfo = (object) => {
-  const itemInfo = {
-    id: object.id,
-    title: object.title,
-    salePrice: object.price,
-  };
-  createCartItemElement(itemInfo);
-};
-
-const createCartItemElement = ({ id: sku, title: name, price: salePrice }) => {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-};
-
+// Get info from de second API
+// !important for the second required
 const fetchItem = (param) => {
   fetch(`https://api.mercadolibre.com/items/${param}`)
     .then((response) => response.json())
     .then((product) => {
+      // eslint-disable-next-line no-use-before-define
       getItemInfo(product);
     });
 };
 
-const cartItemClickListener = () => {
+// When the button is listened, the id is get and send to a second API
+const cartItemClickListener = (func) => {
   const cartItemClick = document.querySelectorAll('.item__add');
-  console.log(cartItemClick);
   cartItemClick.forEach((_, index) => {
     cartItemClick[index].addEventListener('click', () => {
       const getSku = document.querySelectorAll('.item__sku');
       const nome = getSku[index].innerText;
       console.log(`https://api.mercadolibre.com/items/${nome}`);
-      fetchItem(nome);
+      // eslint-disable-next-line sonarjs/no-extra-arguments
+      fetchItem(nome, func);
     });
   });
 };
+
+const createCartItemElement = ({ id: sku, title: name, price: salePrice }, funct) => {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', funct);
+  return li;
+};
+
+function getItemInfo(object) {
+  const itemInfo = {
+    id: object.id,
+    title: object.title,
+    salePrice: object.price,
+  };
+  createCartItemElement(itemInfo, cartItemClickListener);
+}
 
 const getProductInfo = (object) => (object.results.forEach((info) => {
     const objectInfo = {
@@ -92,4 +99,5 @@ window.onload = () => {
   fetchPCList();
 };
 
-const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
+const getSkuFromProductItem = () => console.log('oi');
+getSkuFromProductItem();
