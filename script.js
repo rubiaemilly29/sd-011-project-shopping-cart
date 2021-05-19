db4265479dc6a640255be688ced0d0d8b09a9bc4
Michel Pereira <michelbecauro@gmail.com>
2021-05-19 09:22:34 -0300

    feature/requisito_2/teste addToCart(): Click Botões

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+window.onload = function onload() { 
+
 // const fetch = require('node-fetch'); // Isso parece não ser necessário.
 
 async function createProductImageElement(imageSource) {
@@ -26,9 +29,9 @@ async function createProductItemElement(sku, name, image) { // Modificado aqui. 
   return section;
 }
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
+// function getSkuFromProductItem(item) { // Pega um nó (parametro) e...  
+//   return item.querySelector('span.item__sku').innerText; // ...retorna o texto do elemento (filho entre parenteses)
+// } //<< Acho que esse return é passada como dado de pesquisa p/ API do REQUISITO 2
 
 // function cartItemClickListener(event) {
 //   // coloque seu código aqui
@@ -42,8 +45,29 @@ async function createProductItemElement(sku, name, image) { // Modificado aqui. 
 //   return li;
 // }
 
-// Meu códido aqui ABAIXO também:
 
+
+// [Meu CÓDIGO 1 aqui ABAIXO]:>>
+function addToCart(aqui) {
+  alert('Entrou na addToCart()'); // TESTE
+  console.log("Var de produto recebida baixo:");
+  // console.log(aqui.childNodes);
+  const btn = aqui.childNodes;
+  console.log(btn);
+
+    aqui.addEventListener('click', function (event) { // Detecta em qual item foi clicado e trata algo com o target retornado
+      const clicado = event.target;
+      if (clicado.className === 'item__add') {
+      alert('clicou NO botão heim ;-)');
+      } else {
+        alert('clicou FORA do botão :-(');
+      }
+    });
+   
+  }// <<:[Meu CÓDIGO 1 aqui ACIMA]
+
+
+// [Meu CÓDICO 2 aqui ABAIXO]:>>
 async function getProductList(productType) { // Criei essa função como PROMISE
   if (productType === 'computador') {
     const param = { headers: { Accept: 'application/json' } };
@@ -51,19 +75,26 @@ async function getProductList(productType) { // Criei essa função como PROMISE
    .then((r) => { 
      r.json()
       .then((resolve) => { // Resposta da APi já transforamda em JSON
-        const produto = document.querySelector('.container .items');
+        var produto = document.querySelector('.container .items');
         resolve.results.forEach(async (element) => { // Filtra para percorrer apenas o array de objetos que são cada produto.
           const item = await createProductItemElement(element.id, element.title, element.thumbnail);
           produto.appendChild(item);
-        });          
-      }); // >> Fim do segundo THEN        
-    }); // >> Fim do primeiro THEN  
+        }); 
+        
+        return produto; // TESTANDO.....................RETORNO segundo THEN...
+
+    }) // TESTANDO.....................FIM 2 do segundo THEN...
+      .then((proRetorno) => addToCart(proRetorno));  // TESTANDO...................       
+    }); // >> Fim do primeiro THEN 
+    
+    
   }
   throw new Error('Pequisa só com a palavra "computador"');
 }
+// <<:[Meu CÓDIGO 2 aqui ACIMA]
 
-// :Meu códido aqui ACIMA também:
 
-window.onload = function onload() { 
-getProductList('computador');
-};
+  // addToCart();
+  getProductList('computador');
+
+} // FIM do window.onload
