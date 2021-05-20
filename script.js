25ce6d41b3c16f114d992292a8ee6d8b8e30c818
heyynat <natalifplima@gmail.com>
2021-05-20 12:07:14 -0400

    Requisito 4 e 7. e lint

--- script.js
+++ script.js
@@ -24,18 +24,19 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
 // Requisito 5
 function payment() {
   const payout = document.querySelector('.total-price');
   const listItems = [...document.querySelectorAll('.cart__item')];
   payout.innerText = 0;
-  const sum = listItems.reduce((acc, value) => acc + Number(value.innerText.split('PRICE: $')[1]), 0);
-  payout.innerText = `Total a pagar: $${sum}`;
-};
+  const sum = listItems.reduce((acc, cv) => acc + Number(cv.innerText.split('PRICE: $')[1]), 0);
+  // payout.innerText = `Total a pagar: $${sum}`;
+  payout.innerText = sum;
+}
 
 // Requisito 3
 function cartItemClickListener(event) {
@@ -51,32 +52,73 @@ function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   return li;
 }
 
-// Requisito 1
-const apiUrl = "https://api.mercadolibre.com/sites/MLB/search?q=computador";
-
-function listItems() {
-  fetch(apiUrl)
-  .then((r) => r.json())
-  .then((r) => r.results.forEach((item) => {
-    const itemToFind = { sku: item.id, name: item.title, image: item.thumbnail };
-    document.querySelectorAll('.items')[0].appendChild(createProductItemElement(itemToFind));
-  })) .then(() => addProductToCart())
-  .catch((err)=>console.log(err));
-}
-
 // Requisito 2
 function addProductToCart() {
-  const cartItems = document.querySelector('.cart__items');
   const btnAddItems = document.querySelectorAll('.item__add');
   btnAddItems.forEach((element) => element.addEventListener('click', () => {
     const idItem = element.parentElement.firstChild.innerText;
     fetch(`https://api.mercadolibre.com/items/${idItem}`)
     .then((resp) => resp.json())
-    .then((data) => cartItems.appendChild(createCartItemElement(data)))
+    .then((data) => {
+      const ol = document.querySelector('ol');
+      const count = ol.childElementCount;
+      localStorage.setItem('TotalItems', `${count}`);
+      localStorage.setItem(`Item${count}`, 
+      `SKU: ${data.id} | NAME: ${data.title} | PRICE: $${data.price}`);
+      document.querySelector('.cart__items').appendChild(createCartItemElement(data));
+    })
     .then(() => payment());
   }));
 }
 
-window.onload = function onload() { 
+// Requisito 1
+const apiUrl = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+
+function listItems() {
+  fetch(apiUrl)
+  .then((response) => response.json())
+  .then((response) => response.results.forEach((item) => {
+    const itemToFind = { sku: item.id, name: item.title, image: item.thumbnail };
+    const items = document.querySelector('.items');
+    items.appendChild(createProductItemElement(itemToFind));
+  }))
+  .then(() => addProductToCart())
+  .then(() => {
+    document.querySelector('.loading').remove();
+    console.log(document.querySelector('.loading'));
+  })
+  .catch((err) => console.log(err));
+}
+
+// Requisito 6
+function clearCart() {
+  const btnClear = document.querySelector('.empty-cart');  
+  btnClear.addEventListener('click', () => {
+    const payout = document.querySelector('.total-price');
+    const cartToCart = document.querySelector('.cart__items');
+    cartToCart.innerHTML = '';
+    payout.innerHTML = 0;
+    payout.innerText = '';
+    localStorage.clear();
+  });
+}
+
+// Requisito 4
+function backupListItem() {
+  if (localStorage.getItem('TotalItems') > 0) {
+    for (let index = 0; index <= localStorage.getItem('TotalItems'); index += 1) {
+      const cartItems = document.querySelector('.cart__items');
+      const li = document.createElement('li');
+      li.className = 'cart__item';
+      cartItems.appendChild(li);
+      li.innerText = localStorage.getItem(`Item${index}`);
+    }
+  }
+}
+
+window.onload = function onload() {
+  backupListItem();
   listItems();
+  payment();
+  clearCart();
 };
