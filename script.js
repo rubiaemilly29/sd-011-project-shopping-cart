393c4cbe3df654d2e3877471f19920e66c1feb33
JoaoCabrera <joaocabrerati@gmail.com>
2021-05-20 09:13:14 -0300

    Adiciona requisito 2 e 3

--- script.js
+++ script.js
@@ -12,7 +12,21 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function cartItemClickListener(event) {
+  event.target.remove();
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const ol = document.querySelector('.cart__items');
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  ol.appendChild(li);
+  return li;
+}
+
+function createProductItemElement({ sku, name, image, salePrice }) {
   const items = document.querySelector('.items');
   const section = document.createElement('section');
   section.className = 'item';
@@ -20,7 +34,8 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'))
+  .addEventListener('click', () => createCartItemElement({ sku, name, salePrice }));
   items.appendChild(section);
 
   return section;
@@ -30,23 +45,12 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
 function simplifyProduct(computers) {
   return {
     sku: computers.id,
     name: computers.title,
     image: computers.thumbnail,
+    salePrice: computers.price,
   };
 }
 
