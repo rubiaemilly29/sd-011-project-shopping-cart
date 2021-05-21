d6d154763916511225c765d7d57f7e3d3fdbb4bf
Bernardo Prado <bernardoprado22@yahoo.com.br>
2021-05-21 16:45:03 -0300

    Corrige Lint

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
-//configura o elemento
+// configura o elemento
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
