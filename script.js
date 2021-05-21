213db6de89bd4406be3e30809da1736d76e66cf1
nandovbr <nandovbr15@hotmail.com>
2021-05-21 19:01:05 -0300

    requisito 2

--- script.js
+++ script.js
@@ -28,16 +28,6 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-// Meu código
-async function productList() {
-  const section = document.querySelector('.items');
-  const data = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
-  const products = await data.json();
-  products.results.forEach((itens) => {
-    section.appendChild(createProductItemElement(itens));
-  });
-}
-
 function cartItemClickListener(event) {
   // coloque seu código aqui
 }
@@ -48,6 +38,48 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
+}
+
+// Meu código
+async function addToCart(itemId) {
+  const items = document.querySelector('.cart__items');
+  const data = await fetch(`https://api.mercadolibre.com/items/${itemId}`);
+  const product = await data.json();
+  const object = {
+    sku: product.id,
+    name: product.title,
+    salePrice: product.price,
+  };
+  items.appendChild(createCartItemElement(object));
+}
+
+// Meu código
+function clickEventToCart() {
+  const buttons = document.querySelectorAll('.item__add');
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      // descobrir o id do item clicado e chamar o addToCart passando o ID
+      const sku = getSkuFromProductItem(button.parentElement);
+      addToCart(sku);
+      console.log(button.parentElement);
+    });
+  });
+}
+
+// Meu código
+async function productList() {
+  const section = document.querySelector('.items');
+  const data = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  const products = await data.json();
+  products.results.forEach((item) => {
+    const object = {
+      sku: item.id,
+      name: item.title,
+      image: item.thumbnail,
+    };
+    section.appendChild(createProductItemElement(object));
+  });
+  clickEventToCart();
 }
 
 window.onload = function onload() {
