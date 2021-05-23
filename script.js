72b997fbda9965fa3c2451002464356c3be4944f
Bernardo Coelho <bernardocezario21@gmail.com>
2021-05-23 16:07:51 -0300

    Complete exercise 3

--- script.js
+++ script.js
@@ -28,9 +28,7 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+const cartItemClickListener = (event) => event.target.remove();
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
