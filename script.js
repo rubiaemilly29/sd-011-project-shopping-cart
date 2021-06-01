4a718ef26cd753a1e70240fd4c6c2213809fb473
Lucas Henrique Brito <lucas.silva.brito@hotmail.com.br>
2021-06-01 05:59:47 -0300

    Requisito 3

--- script.js
+++ script.js
@@ -28,15 +28,15 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+const cartItemClickListener = (event) => {
+  event.target.remove();
+};
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', (event) => cartItemClickListener(event));
   return li;
 }
 
